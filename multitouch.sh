e4cba6dbb65557356afc7b1140bd6628ad932890
Ramón Cahenzli <rca@psy-q.ch>
2011-07-12 10:19:02 +0200

    Multitouch enabling on Debian GNU/Linux with Synaptics touchpads.

---
+++ multitouch.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+xinput set-int-prop "SynPS/2 Synaptics TouchPad" "Synaptics Two-Finger Pressure" 32 10
+xinput set-int-prop "SynPS/2 Synaptics TouchPad" "Synaptics Two-Finger Width" 32 8
+xinput set-int-prop "SynPS/2 Synaptics TouchPad" "Two-Finger Scrolling" 8 1
+xinput set-int-prop "SynPS/2 Synaptics TouchPad" "Synaptics Two-Finger Scrolling" 8 1 1
+
