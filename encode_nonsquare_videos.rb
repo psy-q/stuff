ae7d16fa04d31c5d8cdf96752261585540510ceb
Ramón Cahenzli <rca@psy-q.ch>
2011-09-27 10:41:54 +0200

    New extra-fancy way to try and determine the PAR/DAR/SAR.

--- encode_nonsquare_videos.rb
+++ encode_nonsquare_videos.rb
@@ -61,6 +61,12 @@ class Ffmpeg
   # How many threads to use -- speeds things up on multi-core/multi-CPU machines
   @@threads = 0
 
+  def self.extract_sar(filename)
+    # DANGER: Assumes that the video is at least 60 seconds long, and tries to grab some frame from the middle to reliably
+    # determine aspect ratio
+    sample_aspect_ratio = `ffmpeg -vf showinfo -ss 60 -t 1 -i #{filename} -y /tmp/temp.m4v 2>&1 | grep "sar:" | grep -v sws_param | cut -d " " -f 9 | uniq`
+  end
+
   def self.encode(filename, destdir = "./")
     commands = self.build_commands(filename, destdir)
     commands.each do |command|
@@ -72,7 +78,8 @@ class Ffmpeg
   def self.build_commands(filename, destdir)
     commands = []
     output_basename = File.basename(filename, File.extname(filename))
-    aspect_ratio = Mplayer.new(filename).info[:video_aspect]
+    #aspect_ratio = Mplayer.new(filename).info[:video_aspect]
+    aspect_ratio = Ffmpeg.extract_sar(filename).gsub("/",":")
     deinterlace = '-vf "yadif=3"'
     video_codec = "-vcodec libx264"
     audio_codec = "-acodec libfaac -ab 160k"
@@ -86,7 +93,7 @@ class Ffmpeg
     #commands << "/usr/local/bin/ffmpeg -i #{filename} #{deinterlace} -threads #{@@threads} #{audio_codec} #{destdir}/#{output_basename}.aac"
 
     # This command encodes without audio to an MP4 container
-    commands << "/usr/local/bin/ffmpeg -i #{filename} #{deinterlace} #{video_codec} #{audio_codec} -threads #{@@threads} #{quality} #{destdir}/#{output_basename}.m4v"
+    commands << "/usr/local/bin/ffmpeg -i #{filename} #{deinterlace} #{video_codec} #{audio_codec} -threads #{@@threads} #{quality} -aspect #{aspect_ratio} #{destdir}/#{output_basename}.m4v"
     
     return commands
     
